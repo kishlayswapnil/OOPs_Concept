94386a72113bb12a7f6bc8dabed1c4ada59ae4a8
kishlayswapnil <kishlayswapnil@gmail.com>
2020-03-14 10:18:50 +0530

    Refactored: getWeight method has been added.

--- OOPS_Concepts/src/main/java/com/bridgelabz/inventory_data_management/Inventory.java
+++ OOPS_Concepts/src/main/java/com/bridgelabz/inventory_data_management/Inventory.java
@@ -3,17 +3,25 @@ package com.bridgelabz.inventory_data_management;
 //A Basic structure of what details will be entered to the json file.
 public class Inventory {
 	private String name;
+	private double weight;
 	private double price;
 	
 	//Method to add data in json file.
-	public Inventory(String name, double price) {
+	public Inventory(String name,double weight, double price) {
 		this.name = name;
+		this.weight=weight;
 		this.price =price;
 	}
 	
 	//Method to get the name from json file.
 	public String getName() {
 		return name;
+	}
+	
+	//Method to get the weight from the json file.
+	public double getWeight()
+	{
+		return weight;
 	}
 	
 	//Method to get the price from json file.
