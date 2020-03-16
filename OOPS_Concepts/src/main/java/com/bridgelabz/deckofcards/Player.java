abb66ad3f718b8fba002598ac4be89dc9f220a50
kishlayswapnil <kishlayswapnil@gmail.com>
2020-03-16 12:28:16 +0530

    Added: The player class for creating one player object has been added.

---
+++ OOPS_Concepts/src/main/java/com/bridgelabz/deckofcards/Player.java
@@ -0,0 +1,46 @@
+package com.bridgelabz.deckofcards;
+
+import com.bridgelabz.utility.LinkedListUsingQueue;
+
+//Implementing the Cards interface for the methods.
+public class Player implements Cards {
+	
+	//Declarations.
+	int index=0;
+	String cards[];
+	String rank[]={"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+	public LinkedListUsingQueue queue=new LinkedListUsingQueue();
+	
+	//This method will help in creating the object array in main class.
+	public Player()
+	{
+		cards=new String[9];
+		for(int index=0;index<9;index++)
+			cards[index]=new String();
+	}
+	
+	//Enqueueing the sorted data in cards.
+	public void getCards(String card)
+	{
+		
+		cards[index]=card;
+		index++;
+		if(index==9)
+			sort();
+	}
+	
+	//Enqueueing the sorted value in the linked list.
+	public void sort()
+	{
+		for(int index1=0;index1<rank.length;index1++)
+		{
+			for(int index2=0;index2<cards.length;index2++)
+			{
+				if(cards[index2].charAt(0)==rank[index1].charAt(0))
+				{
+					queue.enqueue(cards[index2]);
+				}
+			}
+		}
+	}
+}
